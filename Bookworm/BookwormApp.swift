b87db8caee69fcdf2ac62c7c2063e8de9a6f7a9f
Ojidaemo <vit.martsinovich@gmail.com>
2023-09-12 16:22:44 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Vitali Martsinovich on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
